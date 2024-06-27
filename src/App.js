94b974be79debefa1322de508447fec0bce5c4ac
Bonor Ayambem <bonorayambem@gmail.com>
2024-06-27 18:28:48 -0400

    setup + header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import globe from './globe.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+export default function App(){
+  return(
+    <div className='container'>
+      <header className='header'>
+        <img src={globe} alt='globe icon' className="globe-icon"/>
+        <p className="title">my travel journal</p>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
